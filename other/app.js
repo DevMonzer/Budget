287707bd0d247bcf433988f77fbc7792a457e9ad
DevMonzer <dev.monzer@gmail.com>
2020-12-10 09:43:08 +0200

    added some js

--- other/app.js
+++ other/app.js
@@ -5,7 +5,7 @@ var budgetController  = (function() {
 
 })();
 
-// UI CONTROLLER
+// UI CONTROLLER 
 var UIController = (function() {
 
     // Some code
@@ -15,6 +15,27 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var ctrlAddItem = function() {
+
+        // 1- Get the field input data 
+
+        // 2- Add the item to the budget controller 
+        
+        // 3- Add the item to the UI 
+        
+        // 4- Calculate the budget 
+        
+        // 5- Display the budget on the UI
+
+        console.log("it works");
+    }
     
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
     
 })(budgetController, UIController);
